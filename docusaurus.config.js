012e120edec3060c5770a058d66cfc6618054184
Nora <143967799+NoraIntel@users.noreply.github.com>
2023-09-06 12:16:10 +0200

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,13 +45,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        outils: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
@@ -84,7 +77,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/outils', label: 'outils', position: 'left'},
+          {to: '/docs/outils', label: 'Outils', position: 'left'},
           {to: '/blog', label: 'Tutorial', position: 'left'},
           {
             href: 'https://github.com/NoraIntel',
