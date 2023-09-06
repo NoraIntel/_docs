7c306f3f20c10d58f43a7c03692cc5b497102fdb
Nora <143967799+NoraIntel@users.noreply.github.com>
2023-09-06 12:31:31 +0200

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,14 +94,14 @@ const config = {
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
-      announcementBar: {
-        id: 'support_us',
-        content:
-          'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-        backgroundColor: '#fafbfc',
-        textColor: '#091E42',
-        isCloseable: false,
-      },
+      // announcementBar: {
+      //   id: 'support_us',
+      //   content:
+      //     'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+      //   backgroundColor: '#fafbfc',
+      //   textColor: '#091E42',
+      //   isCloseable: false,
+      // },
       footer: {
         style: 'light',
         links: [
